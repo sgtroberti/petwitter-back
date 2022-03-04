6a38b63b2f2f0a0c078af6062090ebd5ddcde970
Guilherme Roberti <guimourinha@gmail.com>
2022-03-03 21:31:52 -0300

    heroku configs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import app from "./config/server.js";
+import { envs } from "./helpers/utils.js";
 
-app.listen(5000, (err) => {
+app.listen(envs.PORT, "0.0.0.0", (err) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   console.log(`
-  🚀 Server ready at: http://localhost:5000
+  🚀 Server ready at: http://localhost:${envs.PORT}
   ⭐️ See sample requests: http://pris.ly/e/ts/rest-fastify#3-using-the-rest-api`);
 });
